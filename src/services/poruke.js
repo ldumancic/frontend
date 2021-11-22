bcd7bb0384d3a9da0fe61a4337011bcd305b1060
ldumancic <92321895+ldumancic@users.noreply.github.com>
2021-11-22 17:08:13 +0100

    azuriranje adresa API posluzitelja

--- src/services/poruke.js
+++ src/services/poruke.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const osnovniUrl = 'http://localhost:3001/api/poruke'
+const osnovniUrl = 'https://poruke-backend-ld.herokuapp.com/api/poruke'
  
 const dohvatiSve = () => {   
     return axios.get(osnovniUrl);
